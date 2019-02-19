3b3ea8eca6cded728e367cd4cf78b1fb01439bd2
Ryan Sharkey <sharkeyryan@gmail.com>
2019-02-18 22:12:23 -0800

    Add delete item component logic

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,6 +1,12 @@
 class AllItems extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(id) {
+    this.props.handleDelete(id);
   }
 
   render() {
@@ -9,6 +15,7 @@ class AllItems extends React.Component {
         <div key={item.id}>
           <h3>{item.name}</h3>
           <p>{item.description}</p>
+          <button onClick={this.handleDelete(item.id)}>Delete</button>
         </div>
       )
     });
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -6,6 +6,8 @@ class Body extends React.Component {
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.removeItemClient = this.removeItemClient.bind(this);
   }
 
   componentDidMount() {
@@ -20,11 +22,47 @@ class Body extends React.Component {
     this.setState({ items: newState })
   }
 
+  handleDelete(id) {
+    const options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" }
+    };
+
+    fetch(`/api/v1/items/${id}`, options)
+    .then(function(response) {
+      if(response.ok){
+        return response.json();
+      } else {
+        throw new Error("Delete Failed")
+      }
+    })
+    .then(function(responseBody){
+      return responseBody;
+      console.log(JSON.stringify(responseBody));
+    }).
+    then((id) => {
+      this.removeItemClient(id);
+    })
+    .catch(function(error) {
+      console.log("Delete failed", error);
+    });
+  }
+
+  removeItemClient(id) {
+    var newItems = this.state.items.filter((item) => {
+      return item.id != id;
+    });
+
+    this.setState({ items: newItems });
+  }
+
   render() {
     return (
       <div>
         <NewItem handleSubmit={this.handleSubmit} />
-        <AllItems items={this.state.items}/>
+        <AllItems
+          items={this.state.items}
+          handleDelete={this.handleDelete} />
       </div>
     )
   }
